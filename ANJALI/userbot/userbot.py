984be953b456a37d0e304d6a62c2b03fde46a129
VENOM <venommusic91@gmail.com>
2025-01-26 20:24:20 +0530

    Update userbot.py

--- ANJALI/userbot/userbot.py
+++ ANJALI/userbot/userbot.py
@@ -9,17 +9,17 @@ import config
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="VIPAss1",
+            name="ANJALIAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=False,
-            plugins=dict(root="ANJALI.idchatbot"),
+            plugins=dict(root="ANJALI.ANJALI"),
         )
         
 
     async def start(self):
-        print(f"Starting Id chatbot...")
+        print(f"Starting ANJALI...")
 
         if config.STRING1:
             await self.one.start()
@@ -35,12 +35,12 @@ class Userbot(Client):
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
      
-            print(f"Id-Chatbot Started as {self.one.me.first_name}")
+            print(f"ANJALI Started as {self.one.me.first_name}")
             
         
 
     async def stop(self):
-        LOGGER(__name__).info(f"Stopping Id-Chatbot...")
+        LOGGER(__name__).info(f"Stopping ANJALI...")
         try:
             if config.STRING1:
                 await self.one.stop()
