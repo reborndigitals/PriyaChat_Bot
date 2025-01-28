12f4d3002af7aa6c3db080a5351e28df9f9818e3
VENOM <venommusic91@gmail.com>
2025-01-28 14:20:38 +0530

    Update __main__.py

--- ANJALI/__main__.py
+++ ANJALI/__main__.py
@@ -56,12 +56,10 @@ async def anony_boot():
                 BotCommand("chatlang", "Get current using lang for chat"),
                 BotCommand("resetlang", "Reset to default bot reply lang"),
                 BotCommand("id", "Get users user_id"),
-                BotCommand("stats", "Check bot stats"),
                 BotCommand("gcast", "Broadcast any message to groups/users"),
                 BotCommand("chatbot", "Enable or disable chatbot"),
                 BotCommand("status", "Check chatbot enable or disable in chat"),
                 BotCommand("shayri", "Get random shayri for love"),
-                BotCommand("ask", "Ask anything from chatgpt"),
                 BotCommand("repo", "Get chatbot source code"),
             ]
         )
