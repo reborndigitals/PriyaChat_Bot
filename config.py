90bae5f2ccfb3506c38ed21d80c9b66429640bef
reborndigitals <reborndigitals@gmail.com>
2025-07-24 14:09:01 +0530

    config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "24996524"
+API_ID = "10284859"
 # -------------------------------------------------------------
-API_HASH = "b0825fc3f523fd28b2ea1735a07a058e"
+API_HASH = "b0ad58eb8b845ba0003e0d9ce5fc2196"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1073815732"))
-SUPPORT_GRP = "Music_World_Support"
-UPDATE_CHNL = "Music_World_Update"
-OWNER_USERNAME = "MR_ROCKY_TZ"
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(getenv("OWNER_ID", "1281282633"))
+SUPPORT_GRP = "HeartBeat_Fam"
+UPDATE_CHNL = "HeartBeat_Offi"
+OWNER_USERNAME = "rajeshrakis"
