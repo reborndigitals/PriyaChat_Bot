7cb3cb11b46f70af998c2893bc12b75aadca7ba6
reborndigitals <reborndigitals@gmail.com>
2025-07-24 15:21:39 +0530

    userbot.py

--- ANJALI/userbot/userbot.py
+++ ANJALI/userbot/userbot.py
@@ -19,15 +19,15 @@ class Userbot(Client):
         
 
     async def start(self):
-        print(f"Starting ANJALI...")
+        print(f"Starting HEARTBEAT...")
 
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Music_World_Support")
-                await self.one.join_chat("Music_World_Support")
-                await self.one.join_chat("Music_World_Support")
-                await self.one.join_chat("Music_World_Support")
+                await self.one.join_chat("HeartBeat_Fam")
+                await self.one.join_chat("HeartBeat_Fam")
+                await self.one.join_chat("HeartBeat_Fam")
+                await self.one.join_chat("HeartBeat_Fam")
 
             except:
                 pass
@@ -35,12 +35,12 @@ class Userbot(Client):
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
      
-            print(f"ANJALI Started as {self.one.me.first_name}")
+            print(f"HEARTBEAT Started as {self.one.me.first_name}")
             
         
 
     async def stop(self):
-        LOGGER(__name__).info(f"Stopping ANJALI...")
+        LOGGER(__name__).info(f"Stopping HEARTBEAT...")
         try:
             if config.STRING1:
                 await self.one.stop()
