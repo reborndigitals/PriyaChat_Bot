dc249b08a780f511bd1871bd1d9a03d57fb097c6
reborndigitals <reborndigitals@gmail.com>
2025-07-26 07:06:44 +0530

    config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = "10284859"
 API_HASH = "b0ad58eb8b845ba0003e0d9ce5fc2196"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "7956556231:AAEgPQOQsvTqpFV5I6BuFBCcPn-z_RGaKgc")
-STRING1 = getenv("STRING_SESSION", "BQB6w5MApuc7wvpp1dWv2vFHUNpLRoCWls8GG7xDWW01ZSqeCbPadwYpgIHdfUWrg1n_4W5_NFC5himNl3K92y8WdSIfX2rff_TbTMP_GBTenc1AH1tP8c71hDGsfXuh2AUXLbPoU2FIYPONKilXmtJUpIGIbfz4lEEmpF55hB9rGDSglbL9If43OhOuYviSBJy1ep3fOeIii4g8142yXrOc7raS7aqnAoO_xHlWC7PPMHh9c6QedIE5ZkjyIcLEh2SSMndz7cz2N_zJfsJHokFvRCs0wlpefC4JZV18aHV70e-dqVK_VN6-72W-7NJv068ZyvQU6C6JglAreGbapnDPUtj36wAAAAHtIoKoAQ")
+STRING1 = getenv("STRING_SESSION", "BQGVn2EAP_P3wNGw5EC62KWf66UWqO8_snHTIoT2qOBxqG7slnLMLAbEbPDlPIA_GDDkUxYAgI3DMABtwDiB8wLrJ10OmjgNuLCVeBvYcA13VYsryydjl5WL6XUbH17SQ7dZEDPsdNDdFazAnSNivsVIYoytJB61bkFAajDkmaTKOlYUKy0XA5si2Xhmew-kWen8pVSazgQKcNRxgtbIJdQD_B_zzUMMSn4aMS1Nj6B_9D5pYnh8jUo78yk-jc2svuYmYqkRFUv5TCSnW08hiXdY-OQ6X-9o1s6OOAzvvdy2M4m781O0uQxVPcwDYDiRL2P3vnIDttkEtxbPgBqEhHkAHY9DOQAAAAGkzbnBAA")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "1281282633"))
 SUPPORT_GRP = "HeartBeat_Fam"
