f6d44b05c5b3e8c83a977ea1b7eb8a3537e25321
reborndigitals <reborndigitals@gmail.com>
2025-07-26 15:51:53 +0530

    config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "b0ad58eb8b845ba0003e0d9ce5fc2196"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "7956556231:AAEgPQOQsvTqpFV5I6BuFBCcPn-z_RGaKgc")
 STRING1 = getenv("STRING_SESSION", "BQB6w5MAlZzy78qKuEUzApUyMzAFuo9Hrl-2q2JbFm-mqMkpefh1n6WiIsAzjM4rZsMA1Vk8SJtDntcmbaTm2ktjzzzIUs6a8PBg8I-bYM35HJ3fNQWJgshoBXvNz9ttETNdfBuBhX7UPcJMjmiJoftuTFwnBroShOGHMuI-gQ-TDxEBQkhKDE0kErrJN8bKtVWr91DhbS97o6I_dPtb-MxPJdsiMpDD4dXhJePKXd_QDlIIr8sqLZ4P7pInti5GPeZuDF9QeGxe43mQwD_ommPiVkMIZltjEN46UfJJ4IxOfcZdgKgEbl4cPOYRbKiyTqh6KGC-U19AcaSLCGkk7q4Nd2uN9gAAAAHcYK2wAQ")
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://ghosttbatt:Ghost2021@ghosttbatt.ocbirts.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "1281282633"))
 SUPPORT_GRP = "HeartBeat_Fam"
 UPDATE_CHNL = "HeartBeat_Offi"
