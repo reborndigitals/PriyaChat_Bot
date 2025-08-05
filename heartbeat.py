f747670233da1cee4163194721d16d0e70c27dd5
reborndigitals <reborndigitals@gmail.com>
2025-08-05 15:59:58 +0530

    heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -1,17 +1,18 @@
-# keep_alive.py
+# heartbeat.py
 
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/')
 def home():
     return "Bot is running on Render!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=10000)
 
-def keep_alive():
+def heartbeat():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
