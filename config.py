23bc421e13dc112df5e4321fbc3d01b90c207acf
reborndigitals <reborndigitals@gmail.com>
2025-08-05 16:20:44 +0530

    config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "10284859"
 # -------------------------------------------------------------
 API_HASH = "b0ad58eb8b845ba0003e0d9ce5fc2196"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7956556231:AAEgPQOQsvTqpFV5I6BuFBCcPn-z_RGaKgc")
+BOT_TOKEN = getenv("BOT_TOKEN", "8273429160:AAGCmQY2ww8ygR7Sg2Kt34Pijn36CJV1yZg")
 STRING1 = getenv("STRING_SESSION", "BQB6w5MAYfocT04CIcHAV1UXuiIrkLTPuXthhcvLBFBylZhq58gJOUAZ04IVCbc9KaE_Nz_qqW5-xE1uwpFp7T45xfXBZGHLYVrxWxKUn17iG1XyEWTS03wEkzOZwCssHhx46qmarl8MSq2cIFv4fEGCe2qLvRoxkKtF3VZf-yAWaqQjybI8037SeoS72QDR5DjvVMzaEXbNCdy4mpgZ4maUDjgd-1Hftwb4HvEMwkjoZrggIPlJU7MrzbTGcXKNbQvKE0PK1MG7gubjnjNx6kWgfR2_IH23Dn5swG1b6GrUK3ILfYh961m_aFWyHOoMcgAJe48pG9wRfkjmq5ryQMmZTeOA2gAAAAHgIzFgAQ")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://ghosttbatt:Ghost2021@ghosttbatt.ocbirts.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "1281282633"))
